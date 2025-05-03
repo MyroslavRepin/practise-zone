28b0749d3b7306fc9f4681bb19fcbacc4cabdd24
MyroslavRepin <myroslavrepin@gmail.com>
2025-05-02 18:27:17 -0600

    Finally did day_2. Now it should be working

--- everyday_practise/day_2.py
+++ everyday_practise/day_2.py
@@ -1,16 +1,21 @@
 # This code should find digits and sum them from the strig
 
 def sum_digit(text):
-    total = []
+    total = 0
     current_nums = ""
     for char in text:
         if char.isdigit():
-            current_nums = char
+            current_nums += char
+            print(current_nums)
         else:
-            total.append(current_nums)
-    return sum(total)
+            print('else')
+
+    if current_nums != "":
+        total += int(current_nums)
+
+    return total
 
 
-print(sum_digit("anbvdesn45367e8ducjb4367"))
+print(sum_digit("a100b200c300d400e500"))
 
-
+# FINALLY DONE. THAT'S WAS QUET HARD :)
