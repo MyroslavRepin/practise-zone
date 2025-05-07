f576ca02a9738d8e7ef53acf45f474e74ace56af
MyroslavRepin <myroslavrepin@gmail.com>
2025-05-07 17:13:24 -0600

    Easy done, without copying. It took like 5 minutes.

---
+++ level_up_practise/task_4.py
@@ -0,0 +1,10 @@
+def return_even(raw_list):
+    even_list = []
+    for odd in raw_list:
+        if odd % 2 == 0:
+            even_list.append(odd)
+    return even_list
+
+
+raw_list = [1, 4, 7, 10]
+print(return_even(raw_list))
