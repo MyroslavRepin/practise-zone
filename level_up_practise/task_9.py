bb5b4120341bcbc8a3cd23a253ee4722efa6d512
MyroslavRepin <myroslavrepin@gmail.com>
2025-05-08 17:54:49 -0600

    Finishing checking all the pairs of values

--- level_up_practise/task_9.py
+++ level_up_practise/task_9.py
@@ -1,14 +1,31 @@
 def max_number(raw_list):
     bigger_num = []
     smaller_num = []
-    for digit in raw_list:
-        digit_index = raw_list.index(digit) + 1
-        print(digit_index)
-        print(f"Index 1: {raw_list[digit_index]}")
 
-        # if digit > raw_list[digit_index + 1]:
-        #     print('Bigger')
-        # print(digit)
-"""I should use enmurate(), bocause my code works not properly"""
+    # for digit in raw_list:
+    #     digit_index = raw_list.index(digit) + 1
+    #     print(digit_index)
+    #     print(f"Index 1: {raw_list[digit_index]}")
 
-max_number(raw_list=[10, 2, 3, 4])
+    #     # if digit > raw_list[digit_index + 1]:
+    #     #     print('Bigger')
+    #     # print(digit)
+
+    # for index, digit in enumerate(raw_list):
+    #     if index + 1 > len(raw_list):
+    #         print(f"Bigger: {digit}")
+    #     else:
+    #         print('Less')
+    #     # print(index, digit)
+
+    for i, digit in enumerate(raw_list):
+        if i + 1 < len(raw_list):  # чтобы не выйти за границу
+            # print(f"Текущий: {digit}, Следующий: {raw_list[i + 1]}")
+            if digit > raw_list[i + 1]:
+                print(
+                    f"Bigger  ||  Digit: {digit}  |  Next: {raw_list[i + 1]}")
+            if digit < raw_list[i + 1]:
+                print(f"Less  ||  Digit: {digit}  |  Next: {raw_list[i + 1]}")
+
+
+max_number(raw_list=[10, 2, 1, 4])
