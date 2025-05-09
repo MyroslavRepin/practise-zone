3ce03fb2424a499d64d33d82650d1b11cbce89ab
MyroslavRepin <myroslavrepin@gmail.com>
2025-05-08 22:47:33 -0600

    Done task 9

--- level_up_practise/task_9.py
+++ level_up_practise/task_9.py
@@ -1,13 +1,22 @@
 def max_number(raw_list):
+    # I WAS TRYING TO USE IT, BUT DIDN'T FIGURE OUT.....
+    # for i, digit in enumerate(raw_list[::-1]):
+    #     if i + 1 < len(raw_list):
+    #         if digit > raw_list[i + 1]:
+    #             print(
+    #                 f"Bigger  ||  Digit: {digit}  |  Next: {raw_list[i + 1]}")
+    #             max_list.append(digit)
+    #         if digit < raw_list[i + 1]:
+    #             print(
+    #                 f"Less  ||  Digit: {digit}  |  Next: {raw_list[i + 1]}")
 
-    for i, digit in enumerate(raw_list):
-        if i + 1 < len(raw_list):
-            # print(f"Текущий: {digit}, Следующий: {raw_list[i + 1]}")
-            if digit > raw_list[i + 1]:
-                print(
-                    f"Bigger  ||  Digit: {digit}  |  Next: {raw_list[i + 1]}")
-            if digit < raw_list[i + 1]:
-                print(f"Less  ||  Digit: {digit}  |  Next: {raw_list[i + 1]}")
+    # print(max_list)
+
+    current_max = float("-inf")
+    for number in reversed(raw_list):
+        if number >= current_max:
+            current_max = number
+    print(current_max)
 
 
 max_number(raw_list=[10, 2, 1, 4, 12])
