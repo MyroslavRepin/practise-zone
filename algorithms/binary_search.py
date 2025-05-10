8bbdcd5dbe7a1a14ebeb49fac61fc0ecaf71cc34
MyroslavRepin <myroslavrepin@gmail.com>
2025-05-10 09:54:19 -0600

    Re-doing some logics (experimental)

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,19 +1,15 @@
-def binary_search(sorted_list):
-    central_index = len(sorted_list) // 2
-
-    print(f"First part: {sorted_list[:central_index]}")
-
-    def delete_half(central_index_val, sorted_list):
-        print(f"Will be removed: {sorted_list[central_index_val:]}")
-        sorted_list[central_index_val:].clear()
-
-    delete_half(central_index, sorted_list)
+def binary_search(sorted_list, target):
+    middle = len(sorted_list) // 2
+    last_element = sorted_list[:middle+1][:-1]
+    print(last_element[-1])
+    #     print('Less')
+    # else:
+    #     print('else')
 
 
-searching_number = 7
+target = 7
 
-raw_list = [23, 7, 42, 15, 3, 19, 8, 34, 5, 1, 50, 27]
-sorted_list = sorted(raw_list)
+sorted_list = [1, 2, 3, 4, 5, 6, 7, 8]
 
 print(f"Sorted list: {sorted_list}")
-binary_search(sorted_list)
+binary_search(sorted_list, target)
