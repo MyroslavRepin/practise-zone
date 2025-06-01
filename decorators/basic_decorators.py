039f4d9051237dee9b7977c43779b34b301cbac3
MyroslavRepin <myroslavrepin@gmail.com>
2025-05-31 23:01:25 -0600

    Fix factorial function call example in task_10.py

--- decorators/basic_decorators.py
+++ decorators/basic_decorators.py
@@ -13,8 +13,44 @@ def timer(func):
 @timer
 def set_timer():
     x = 0
-    for i in range(10000):
+    for i in range(7000):
         x += 1
 
 
 print(set_timer())
+
+
+def logger(func):
+    def wrapper():
+        return f"Running function: {func.__name__}"
+    return wrapper
+
+
+@logger
+def test_logger():
+    """MY LOGGER"""
+    print('Testing')
+
+
+print(test_logger())
+
+
+admin_rights = True
+
+
+def check_admin(func):
+    def wrapper():
+        if admin_rights == False:
+            return 'Acces denied'
+        if admin_rights == True:
+            return func()
+    return wrapper
+
+
+@check_admin
+@timer
+def delete_database():
+    return 'Database deleted'
+
+
+print(delete_database())
--- level_up_practise/task_10.py
+++ level_up_practise/task_10.py
@@ -2,4 +2,4 @@ def factorial(n):
     pass
 
     # Example: factorial(5) ---> 120  | 5 * 4 * 3 * 2 * 1
-factorial()
+factorial(5)
