8f979f61b749e4f4f3b07caf0c2fbcef9670fc40
MyroslavRepin <myroslavrepin@gmail.com>
2025-06-07 14:46:48 -0600

    Refactor Bank class to use a dictionary for USERS and improve user management

--- projects/bank/main.py
+++ projects/bank/main.py
@@ -1,10 +1,13 @@
 class Bank:
-    USERS = []
+    USERS = {}
+    counter = 1
 
     def __init__(self, username) -> None:
         self.balance = 0
         self.username = username
-        Bank.USERS.append(self.username)
+        self.varname = f"user{Bank.counter}"
+        Bank.USERS[self.username] = self.varname
+        Bank.counter += 1
 
     def deposit(self, add_funds):
         self.balance += add_funds
@@ -15,26 +18,31 @@ class Bank:
             return "Not enough funds"
         if withdraw <= self.balance:
             self.balance -= withdraw
-            return f'Succesfully sent {withdraw}'
+            return f'Succesfully withdrawed {withdraw}'
 
     def get_balance(self):
         return f'Balance: {self.balance}'
 
     def transfer(self, destination, amount):
-        if destination in self.USERS:
-            for user in self.USERS:
-                if user == destination:
-                    return f'Found user: {user}'
-                else:
-                    'Did not found'
+        if destination in Bank.USERS:
+            user = Bank.USERS[destination]
+            print(f'Found user: {user}')
+            # checking if sender has enough money
+            if amount <= self.balance:
+                self.balance -= amount
+                # TODO: make a transfer
+                # user = Bank.USERS[destination]
+                # user.balance += amount
+                return 'money sent'
+            else:
+                return 'not enough money'
         else:
             return "No such user"
 
 
 user_1 = Bank(username='myroslav')
 user_2 = Bank('artem')
-print(user_1.deposit(147))
-print(user_1.deposit(50))
-print(user_1.withdraw(10))
+user_3 = Bank('ben')
+print(user_1.deposit(add_funds=100))
 print(user_1.get_balance())
-print(user_1.transfer('artem', 10))
+print(user_1.transfer('ben', 10))
