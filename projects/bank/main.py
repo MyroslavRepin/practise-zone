2031834cc5d49ce689663ae7151b11d8ab2374d0
MyroslavRepin <myroslavrepin@gmail.com>
2025-06-09 21:57:31 -0600

    Fix user management in Bank class and improve balance display

--- projects/bank/main.py
+++ projects/bank/main.py
@@ -6,7 +6,7 @@ class Bank:
         self.balance = 0
         self.username = username
         self.varname = f"user{Bank.counter}"
-        Bank.USERS[self.username] = self.varname
+        Bank.USERS[self.username] = self
         Bank.counter += 1
 
     def deposit(self, add_funds):
@@ -18,10 +18,10 @@ class Bank:
             return "Not enough funds"
         if withdraw <= self.balance:
             self.balance -= withdraw
-            return f'Succesfully withdrawed {withdraw}'
+            return f'Succesfully withdrawed: {withdraw}'
 
     def get_balance(self):
-        return f'Balance: {self.balance}'
+        return f'{self.username}\'s Balance: {self.balance}'
 
     def transfer(self, destination, amount):
         if destination in Bank.USERS:
@@ -30,9 +30,7 @@ class Bank:
             # checking if sender has enough money
             if amount <= self.balance:
                 self.balance -= amount
-                # TODO: make a transfer
-                # user = Bank.USERS[destination]
-                # user.balance += amount
+                user.balance += amount
                 return 'money sent'
             else:
                 return 'not enough money'
@@ -46,3 +44,4 @@ user_3 = Bank('ben')
 print(user_1.deposit(add_funds=100))
 print(user_1.get_balance())
 print(user_1.transfer('ben', 10))
+print(user_3.get_balance())
